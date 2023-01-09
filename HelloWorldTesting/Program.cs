55efbc04e0331cbea3703db06d478a899650950a
Evan MacDonald <113189803+Evan-Prime@users.noreply.github.com>
2023-01-09 14:00:09 -0400

    Initial commit

---
+++ HelloWorldTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("press any key to leave.");
+            Console.ReadKey(true);
+        }
+    }
+}
